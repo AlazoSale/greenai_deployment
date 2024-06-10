aeb4a798c2cc97a62f4c8dbdc01a4ebc334a6651
AlazoSale <alazosale2001@example.com>
2024-06-10 17:58:55 +0530

    settings.py update

--- greenai_chatbot_backend/settings.py
+++ greenai_chatbot_backend/settings.py
@@ -143,7 +143,7 @@ WSGI_APPLICATION = 'greenai_chatbot_backend.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600)
 }
 
 '''
