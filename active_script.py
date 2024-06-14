4684a9cec57fc0b3296c0515d76980b109f62665
AlazoSale <alazosale2001@example.com>
2024-06-14 16:23:16 +0530

    tempotp in admin

--- active_script.py
+++ active_script.py
@@ -1,18 +1,22 @@
 import time
 import requests
 
-URL = "https://your-render-service-url.com"
+URL = "https://greenai-deployment.onrender.com/api/chat/ping/"
 
 def ping_service():
     try:
+        start_time = time.time()
         response = requests.get(URL)
+        end_time = time.time()
+        response_time = end_time - start_time
+        
         if response.status_code == 200:
-            print(f"Service is up: {response.status_code}")
+            print(f"Service is up: {response.status_code}, Response time: {response_time:.2f} seconds")
         else:
-            print(f"Service response: {response.status_code}")
+            print(f"Service response: {response.status_code}, Response time: {response_time:.2f} seconds")
     except requests.exceptions.RequestException as e:
         print(f"Error pinging service: {e}")
 
 while True:
     ping_service()
-    time.sleep(6 * 60)
+    time.sleep(0.1 * 60)  # Sleep for 3 minutes
--- user/admin.py
+++ user/admin.py
@@ -3,3 +3,4 @@ from django.contrib import admin
 from . import models as userModels
 # Register your models here.
 admin.site.register(userModels.User)
+admin.site.register(userModels.TempOTP)
