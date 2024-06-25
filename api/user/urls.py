58699852931c00c2fbc245da73f61ac48d8fcc50
Alazo Sale <alazosale2001@gmail.com>
2024-06-25 18:40:15 +0530

    otps

--- api/user/urls.py
+++ api/user/urls.py
@@ -1,14 +1,7 @@
-from rest_framework_simplejwt.views import TokenRefreshView,TokenBlacklistView
 from django.urls import path 
 from . import views 
 urlpatterns = [
-    path('register/request-otp/', views.UserRegisterOTPAPIView.as_view(), name='register'),
-    path('register/', views.UserRegisterAPIView.as_view(), name='register'),
-    path('login/', views.UserLoginAPIView.as_view(), name='login'),
-
-    path('refresh/', TokenRefreshView.as_view(), name='token-refresh'),
-    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
-    path('details/', views.UserDetailsAPIView.as_view(), name='logout'),
+    path('query/', views.QueryAPIView.as_view(), name='query'),
 
 ]
 
